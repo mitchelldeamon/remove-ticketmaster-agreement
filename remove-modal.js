30a9b6d09b4d49820ffed59ba521e4789c602f8b
Mitchell Deamon <mitchdeam@gmail.com>
2024-08-05 15:36:42 -1000

    Create remove-modal.js

---
+++ remove-modal.js
@@ -0,0 +1,4 @@
+javascript:(function() {
+    document.querySelectorAll('.sc-bHnlcS.buZkaS.container--overlay, .sc-kpKSZj.fediLf').forEach(el => el.remove());
+    document.body.style.overflow = 'auto';
+})();
